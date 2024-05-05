5ec3ddf50ef2d1ebc0c9cd339c0c61f26e130645
Igor Dmitrienko <i.dmitrienko@web.de>
2024-05-05 23:43:26 +0200

    Добавить реализацию репозитория книг в памяти

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art of Programming"),
+            new Book(2, "Refactoring"),
+            new Book(3, "C Programming Language"),
+        };
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart))
+                        .ToArray();
+        }
+    }
+}
